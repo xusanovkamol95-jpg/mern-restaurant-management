3bc62d5828eb4253993fa4bf777147a6a553b346
Kamol Xusanov <xusanovk39@gmail.com>
2026-09-14 17:47:03 +0500

    fix cors

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["https://mern-restaurant-frontend.netlify.app", "http://localhost:8765"]
 }))
 
 app.use(express.json())
